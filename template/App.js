129b679bf892e40cc84a599f177c99588ae3c1fd
kiddxu(许晓盛) <kiddxu@tencent.com>
2017-05-17 16:47:27 +0800

    update

--- template/App.js
+++ template/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import AppView from './src';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
-    return (
-      return (<AppView />);
-    );
+    return (<AppView />);
   }
 }
